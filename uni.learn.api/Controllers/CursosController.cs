ed325db6619956a3404f7f7f9b8c58fd6dc86eac
Daniel Salazar <dsnile.salazr@gmail.com>
2025-03-04 00:21:44 -0600

    Add BaseController and CursosController for API structure

---
+++ uni.learn.api/Controllers/CursosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uni.learn.api.Controllers
+{
+
+    public class CursosController : BaseController 
+    {
+        
+    }
+}
